9fada94870cd3cba151a80318a5057079b96dd21
DESKTOP-0KFBH1M\taku4 <taku4kusu@gmail.com>
2024-12-14 20:02:22 +0900

    fix

--- include/klibrary/cybergear.hpp
+++ include/klibrary/cybergear.hpp
@@ -7,12 +7,10 @@ namespace klibrary {
 class Cybergear : public RotaterBase {
 public:
   using RB = RotaterBase;
-  Cybergear(RB::Type rtype, tutrcos::module::Cybergear *cyber,
-            RB::Dir motor_dir, EncoderBase *enc, RB::Dir enc_dir,
-            float reduction_ratio = 1, RB::Turn tmode = RB::Turn::MULTI,
-            float cut_point = 0.5)
-      : RotaterBase{rtype,   cyber,           motor_dir, enc,
-                    enc_dir, reduction_ratio, tmode,     cut_point} {
+  Cybergear(RB::Type rtype, tutrcos::module::Cybergear *cyber, RB::Dir motor_dir, EncoderBase *enc, RB::Dir enc_dir,
+            float reduction_ratio = 1, RB::Turn tmode = RB::Turn::MULTI, float cut_point = 0.5)
+      : RotaterBase{rtype, cyber,    motor_dir, enc, enc_dir, (((rtype == Type::MOTOR) ? (1 / 7.75f) : 1) * reduction_ratio),
+                    tmode, cut_point} {
     cyber_ = cyber;
   }
 
